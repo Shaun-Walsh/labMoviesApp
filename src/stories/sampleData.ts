fe6b0a5ae5415df2e2859a9bc784266c4b6e3431
Shaun Walsh <shaunwalsh24@gmail.com>
2025-08-16 18:43:02 +0100

    Fix broken stories in Storybook.

--- src/stories/sampleData.ts
+++ src/stories/sampleData.ts
@@ -1,149 +1,81 @@
 export default {
-"adult": false,
-"backdrop_path": "/3ombg55JQiIpoPnXYb2oYdr6DtP.jpg",
-"belongs_to_collection": {
-"id": 484312,
-"name": "Skyline Collection",
-"poster_path": "/jaWM0xXSLeb1O2lpfQ9kI0WiGOu.jpg",
-"backdrop_path": "/b2JplRt6qfZKkupnoxl57n3r46y.jpg"
-},
-"budget": 70000000,
-"genres": [
-{
-"id": 878,
-"name": "Science Fiction"
-},
-{
-"id": 28,
-"name": "Action"
-},
-{
-"id": 53,
-"name": "Thriller"
-}
-],
-"homepage": "http://skylin3s.jp/",
-"id": 560144,
-"imdb_id": "tt9387250",
-"origin_country": [
-"US",
-"GB"
-],
-"original_language": "en",
-"original_title": "Skylines",
-"overview": "When a virus threatens to turn the now earth-dwelling friendly alien hybrids against humans, Captain Rose Corley must lead a team of elite mercenaries on a mission to the alien world in order to save what's left of humanity.",
-"popularity": 2.143,
-"poster_path": "/2W4ZvACURDyhiNnSIaFPHfNbny3.jpg",
-"production_companies": [
-{
-"id": 66497,
-"logo_path": null,
-"name": "Mirabelle Pictures Productions",
-"origin_country": ""
-},
-{
-"id": 27758,
-"logo_path": "/je3uCVqOU6CTnbAHRuVQrz8rug4.png",
-"name": "Artbox",
-"origin_country": "LT"
-},
-{
-"id": 146052,
-"logo_path": "/eWrxy5R2izv2WV0zA5cvGq7e2fz.png",
-"name": "Fasten Films",
-"origin_country": "ES"
-},
-{
-"id": 117747,
-"logo_path": "/brD72VA95qgjCtGx0hJEAVUeldW.png",
-"name": "Media Finance Capital",
-"origin_country": "GB"
-},
-{
-"id": 99449,
-"logo_path": null,
-"name": "M45 Productions",
-"origin_country": ""
-},
-{
-"id": 10936,
-"logo_path": null,
-"name": "Hydraulx",
-"origin_country": ""
-},
-{
-"id": 150079,
-"logo_path": null,
-"name": "Gifflar Films Limited",
-"origin_country": ""
-},
-{
-"id": 9987,
-"logo_path": "/o5OTKAw7Acl5fTZYPyl8M8D9570.png",
-"name": "Lipsync Productions",
-"origin_country": "GB"
-},
-{
-"id": 12142,
-"logo_path": "/rPnEeMwxjI6rYMGqkWqIWwIJXxi.png",
-"name": "XYZ Films",
-"origin_country": "US"
-},
-{
-"id": 88606,
-"logo_path": "/9NKtkn1QfC6cqXB107nGox1rgqt.png",
-"name": "Vertical",
-"origin_country": "US"
-},
-{
-"id": 240630,
-"logo_path": null,
-"name": "UAB Cobaltas",
-"origin_country": ""
-},
-{
-"id": 125754,
-"logo_path": null,
-"name": "Cobalt Films",
-"origin_country": ""
-}
-],
-"production_countries": [
-{
-"iso_3166_1": "LT",
-"name": "Lithuania"
-},
-{
-"iso_3166_1": "ES",
-"name": "Spain"
-},
-{
-"iso_3166_1": "AE",
-"name": "United Arab Emirates"
-},
-{
-"iso_3166_1": "GB",
-"name": "United Kingdom"
-},
-{
-"iso_3166_1": "US",
-"name": "United States of America"
-}
-],
-"release_date": "2020-10-25",
-"revenue": 170581,
-"runtime": 113,
-"spoken_languages": [
-{
-"english_name": "English",
-"iso_639_1": "en",
-"name": "English"
-}
-],
-"status": "Released",
-"tagline": "To save our world she must invade theirs.",
-"title": "Skylines",
-"video": false,
-"vote_average": 5.5,
-"vote_count": 562
-}
+  adult: false,
+  backdrop_path: "/5Iw7zQTHVRBOYpA0V6z0yypOPZh.jpg",
+  belongs_to_collection: {
+    id: 10,
+    name: "Star Wars Collection",
+    poster_path: "/iTQHKziZy9pAAY4hHEDCGPaOvFC.jpg",
+    backdrop_path: "/d8duYyyC9J5T825Hg7grmaabfxQ.jpg",
+  },
+  budget: 200000000,
+  genre_ids: [28, 12, 14, 878],
+  genres: [
+    {
+      id: 14,
+      name: "Fantasy",
+    },
+    {
+      id: 12,
+      name: "Adventure",
+    },
+    {
+      id: 878,
+      name: "Science Fiction",
+    },
+    {
+      id: 28,
+      name: "Action",
+    },
+  ],
+  homepage:
+    "https://www.starwars.com/films/star-wars-episode-viii-the-last-jedi",
+  id: 181808,
+  imdb_id: "tt2527336",
+  original_language: "en",
+  original_title: "Star Wars: The Last Jedi",
+  overview:
+    "Rey develops her newly discovered abilities with the guidance of Luke Skywalker, who is unsettled by the strength of her powers. Meanwhile, the Resistance prepares to do battle with the First Order.",
+  popularity: 44.208,
+  poster_path: "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  production_companies: [
+    {
+      id: 1,
+      logo_path: "/o86DbpburjxrqAzEDhXZcyE8pDb.png",
+      name: "Lucasfilm",
+      origin_country: "US",
+    },
+    {
+      id: 11092,
+      logo_path: null,
+      name: "Ram Bergman Productions",
+      origin_country: "US",
+    },
+    {
+      id: 2,
+      logo_path: "/wdrCwmRnLFJhEoH8GSfymY85KHT.png",
+      name: "Walt Disney Pictures",
+      origin_country: "US",
+    },
+  ],
+  production_countries: [
+    {
+      iso_3166_1: "US",
+      name: "United States of America",
+    },
+  ],
+  release_date: "2017-12-13",
+  revenue: 1332459537,
+  runtime: 152,
+  spoken_languages: [
+    {
+      iso_639_1: "en",
+      name: "English",
+    },
+  ],
+  status: "Released",
+  tagline: "Darkness rises... and light to meet it",
+  title: "Star Wars: The Last Jedi",
+  video: false,
+  vote_average: 7,
+  vote_count: 9692,
+};
